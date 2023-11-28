ebbfda73e6ae891896945b1e19ebf43c7459429f
Pontus Lundin <pontus.lundin@yh.nackademin.se>
2023-11-28 15:27:59 +0100

    Started updating GUI

--- src/QuizClient.java
+++ src/QuizClient.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
@@ -9,7 +13,8 @@ import java.util.Scanner;
 public class QuizClient extends JFrame implements Serializable, ActionListener {
 
     private JFrame frame = new JFrame("Quizkampen");
-    private JLabel messageLabel = new JLabel("");
+    private JPanel messagePanel = new JPanel(new FlowLayout());
+    private JTextPane messageArea = new JTextPane();
     private JButton[] categoryButtons = new JButton[3];
     private JButton[] answerButtons = new JButton[4];
 
@@ -31,57 +36,100 @@ public class QuizClient extends JFrame implements Serializable, ActionListener {
         throw new RuntimeException(e);
     }
 }
-        /*
-        {
-
-            messageLabel.setText("Väntar på spelare");
-            JPanel board = new JPanel();
-            frame.getContentPane().add(messageLabel, "North");
-
-
-            //adda knappar här under? eller senare
-
-            frame.getContentPane().add(board, "Center");
-
-            kategori1.setVisible(false);            //knappar
-            kategori2.setVisible(false);
-            kategori3.setVisible(false);
-
-            board.add(kategori1);
-            board.add(kategori2);
-            board.add(kategori3);
-
-
-        } */
-
-
 
     private void initializeUI() {
+        int r = 51;
+        int g = 204;
+        int b = 255;
+        Color custumColor = new Color(r, g, b);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600, 400);
+        frame.setSize(800, 500);
         frame.setResizable(true);
+        frame.setBackground(Color.BLACK);
+        frame.setForeground(Color.BLACK);
 
-        JPanel board = new JPanel();
-        frame.getContentPane().add(board);
+        JPanel boardQuestions = new JPanel(new FlowLayout());
+        boardQuestions.setOpaque(true);
+        boardQuestions.setBackground(custumColor);
+        boardQuestions.setForeground(custumColor);
 
-        messageLabel.setText("Waiting for players...");
-        frame.getContentPane().add(messageLabel, "North");
+        JPanel boardButtons = new JPanel(new GridBagLayout());
+        boardButtons.setOpaque(true);
+        boardButtons.setBackground(custumColor);
+        boardButtons.setForeground(custumColor);
+
+        JLabel boardScore = new JLabel("SCORE: ");
+        boardScore.setOpaque(true);
+        boardScore.setBackground(custumColor);
+        boardScore.setForeground(Color.BLACK);
+
+        //Centerar texten
+        StyledDocument centerMessage = messageArea.getStyledDocument();
+        SimpleAttributeSet center = new SimpleAttributeSet();
+        StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+        centerMessage.setParagraphAttributes(0, centerMessage.getLength(), center, false);
+
+        messageArea.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        messageArea.setText("Waiting for players...");
+        messageArea.setFont(new Font("Serif", Font.PLAIN, 20));
+        //messageArea.setHorizontalAlignment(SwingConstants.CENTER);
+        messageArea.setPreferredSize(new Dimension(550, 100));
+        messageArea.setBackground(Color.WHITE);
+        messageArea.setOpaque(true);
+        messageArea.setEditable(false);
+        //messageArea.setLineWrap(true);
+        //messageArea.setWrapStyleWord(true);
+
+        messageArea.setFocusable(false);
+        messagePanel.add(messageArea);
+        boardQuestions.add(messagePanel, BorderLayout.CENTER);
+
+        frame.getContentPane().add(boardQuestions, "North");
+        frame.getContentPane().add(boardButtons, "Center");
+        frame.getContentPane().add(boardScore, "South");
+
+
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10);
+
 
         for (int i = 0; i < categoryButtons.length; i++) {
             categoryButtons[i] = new JButton();
             categoryButtons[i].setVisible(false);
             categoryButtons[i].addActionListener(this);
-            board.add(categoryButtons[i]);
+            categoryButtons[i].setPreferredSize(new Dimension(150, 75));
+            boardButtons.add(categoryButtons[i], gbc);
+            gbc.gridx++;
+
+            if (gbc.gridx > 1) {
+                gbc.gridx = 0;
+                gbc.gridy++;
+            }
         }
+
+        gbc.gridx = 0;
+        gbc.gridy++;
 
         for (int i = 0; i < answerButtons.length; i++) {
             answerButtons[i] = new JButton();
             answerButtons[i].setVisible(false);
             answerButtons[i].addActionListener(this);
-            board.add(answerButtons[i]);
+            answerButtons[i].setPreferredSize(new Dimension(150, 75));
+            boardButtons.add(answerButtons[i], gbc);
+            gbc.gridx++;
+
+            if (gbc.gridx > 1) {
+                gbc.gridx = 0;
+                gbc.gridy++;
+            }
         }
 
         frame.setVisible(true);
+
     }
 
     public void play() throws Exception {
@@ -101,7 +149,7 @@ public class QuizClient extends JFrame implements Serializable, ActionListener {
             while (true) {
                 response = in.readLine();    //den kommer fortsätta läsa vad servern ger oss, olika alternativ händer beroende på hur spelet utvecklas
                 if (response.startsWith("MESSAGE")) {
-                    messageLabel.setText(response.substring(8));
+                    messageArea.setText(response.substring(8));
                 }
                 else if (response.startsWith("CATEGORY")) {
                     updateCategoryButtons(response.substring(8));
@@ -124,7 +172,8 @@ public class QuizClient extends JFrame implements Serializable, ActionListener {
         for (int i = 0; i < categoryButtons.length; i++) {
             if (i < categoryArray.length) {
                 categoryButtons[i].setVisible(true);
-                categoryButtons[i].setText(categoryArray[i]);
+                categoryButtons[i].setText("<html><center><div style='white-space: nowrap;'>" + categoryArray[i] + "</div></center></html>");
+                categoryButtons[i].setPreferredSize(new Dimension(200, 100));
             } else {
                 categoryButtons[i].setVisible(false);
             }
@@ -136,7 +185,9 @@ public class QuizClient extends JFrame implements Serializable, ActionListener {
         for (int i = 0; i < answerButtons.length; i++) {
             if (i < answerArray.length) {
                 answerButtons[i].setVisible(true);
-                answerButtons[i].setText(answerArray[i]);
+                answerButtons[i].setText("<html><center><div style='white-space: nowrap;'>" + answerArray[i] + "</div></center></html>");
+                answerButtons[i].setPreferredSize(new Dimension(300, 100));
+
             } else {
                 answerButtons[i].setVisible(false);
             }
